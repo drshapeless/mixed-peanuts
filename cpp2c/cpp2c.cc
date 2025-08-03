8e49431b1360494f0855e52fb237cab7097942d7
Jacky Li <drsl@drshapeless.com>
2025-08-03 22:26:15 +0800

    Remove cpp2c

--- cpp2c/cpp2c.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-extern "C" int hello_from_cpp();
-
-int hello_from_cpp() {
-    std::cout << "hello from c++" << std::endl;
-    return 69;
-}
--- cpp2c/main.c
+++
@@ -1,6 +0,0 @@
-#include "cpp2c.h"
-
-int main(int argc, char *argv[]) {
-    hello_from_cpp();
-    return 0;
-}
